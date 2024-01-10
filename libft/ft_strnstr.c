0b1b7f548b61589a8c2f8de8f2e673169092248e
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-10 17:20:06 +0100

    function strnstr

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 16:35:20 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/01/10 17:07:46 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*strnstr(const char *str, const char *find, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	if (*find == '\0')
+		return ((char *)str);
+	while (*str != '\0' && len > 0)
+	{
+		while (str[i] == find[i] && find[i] != '\0' && i < len)
+			i++;
+		if (find[i] == '\0')
+			return ((char *)str);
+		str++;
+		len--;
+	}
+	return (0);
+}
