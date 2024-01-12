70bf489e93c45f428d0ff04d09926d1c919499ef
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-12 20:38:29 +0100

    update

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -12,9 +12,9 @@
 
 #include <strings.h>
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	unsigned	char *ptr;
+	unsigned char	*ptr;
 
 	ptr = (unsigned char *)s;
 	while (n > 0)
