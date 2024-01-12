c012ff42ab16b5b3eaf6584d7e0281c9fed76721
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-12 20:38:46 +0100

    function memcpy

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 20:11:42 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/01/12 20:12:04 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*dest_ptr;
+	const unsigned char	*src_ptr;
+
+	dest_ptr = (unsigned char *)dest;
+	src_ptr = (const unsigned char *)src;
+	while (n > 0)
+	{
+		*dest_ptr = *src_ptr;
+		dest_ptr++;
+		src_ptr++;
+		n--;
+	}
+	return (dest);
+}
