9b702edf028936ad83b4b207a70f1a1c8e45959f
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-13 11:54:50 +0100

    function memchr

---
+++ libft/ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 11:50:20 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/01/13 11:50:23 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*ptr;
+
+	ptr = (const unsigned char *)s;
+	while (n > 0)
+	{
+		if (*ptr == (unsigned char)c)
+			return ((void *)ptr);
+		ptr++;
+		n--;
+	}
+	return (NULL);
+}
