b6b1836f6d912f7c5aa44065712571b161cf6957
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-13 11:39:20 +0100

    function memset

---
+++ libft/ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 11:31:35 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/01/13 11:31:36 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned char	value;
+
+	ptr = (unsigned char *)s;
+	value = (unsigned char)c;
+	while (n > 0)
+	{
+		*ptr = value;
+		ptr++;
+		n--;
+	}
+	return (s);
+}
