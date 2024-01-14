ba5a9a2e0650bafee548c15f32abd76220eb0a99
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-14 14:35:24 +0100

    function substr

---
+++ libft/ft_substr.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 13:54:54 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/01/14 14:34:06 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t	calculate_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+size_t	cal_ef_len(const char *str, size_t start, size_t length)
+{
+	size_t	effective_length;
+
+	effective_length = 0;
+	while (effective_length < length && str[start + effective_length] != '\0')
+		effective_length++;
+	return (effective_length);
+}
+
+char	*allo_and_copy(const char *str, size_t start, size_t effective_length)
+{
+	char	*sub_str;
+	size_t	i;
+
+	sub_str = (char *)malloc(effective_length + 1);
+	i = 0;
+	while (i < effective_length)
+	{
+		sub_str[i] = str[start + i];
+		i++;
+	}
+	sub_str[effective_length] = '\0';
+	return (sub_str);
+}
+
+char	*ft_substr(const char *str, size_t start, size_t length)
+{
+	size_t	str_len;
+	size_t	effective_length;
+
+	if (str == NULL)
+		return (NULL);
+	str_len = calculate_strlen(str);
+	if (start >= str_len || length == 0)
+		return (NULL);
+	effective_length = cal_ef_len(str, start, length);
+	return (allo_and_copy(str, start, effective_length));
+}
