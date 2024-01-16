26421b059acb00493a5abaa051954b74812e09c0
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-16 12:39:43 +0100

    modificada la logica del ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,48 +6,32 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 13:54:54 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/16 11:40:50 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/16 12:38:38 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	cal_ef_len(const char *str, unsigned int start, size_t length)
-{
-	size_t	effective_length;
-
-	effective_length = 0;
-	while (effective_length < length && str[start + effective_length] != '\0')
-		effective_length++;
-	return (effective_length);
-}
-
-char	*allo_and_copy(const char *str, unsigned int start, size_t effe_len)
-{
-	char	*sub_str;
-	size_t	i;
-
-	sub_str = (char *)malloc(effe_len + 1);
-	i = 0;
-	while (i < effe_len)
-	{
-		sub_str[i] = str[start + i];
-		i++;
-	}
-	sub_str[effe_len] = '\0';
-	return (sub_str);
-}
-
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	size_t	str_len;
-	size_t	effective_length;
+	char	*s2;
+	size_t	i;
 
-	if (s == NULL)
+	i = 0;
+	if (!s)
 		return (NULL);
-	str_len = ft_strlen(s);
-	if (start >= str_len || len == 0)
+	if (*s == '\0')
+		return (ft_strdup(""));
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	s2 = malloc(sizeof(char) * len + 1);
+	if (!s2)
 		return (NULL);
-	effective_length = cal_ef_len(s, start, len);
-	return (allo_and_copy(s, start, effective_length));
+	while (i < len)
+	{
+		s2[i] = s[start + i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
 }
