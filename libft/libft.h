550b01bb222109cdb419c02b3e0574ea7f103abc
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-16 12:10:33 +0100

    add todas las funciones hasta hoy

--- libft/libft.h
+++ libft/libft.h
@@ -6,18 +6,43 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:36:04 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/16 11:27:16 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/16 12:08:30 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef LIBFT_H
 # define LIBFT_H
+
 # include <stdlib.h>
 # include <stddef.h>
 # include <stdio.h>
 # include <ctype.h>
 # include <strings.h>
 # include <string.h>
+
+int		ft_isascii(int c);
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t num_elements, size_t element_size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strhr(const char *s, int c);
+char	*ft_strdup(const char *str);
+char	*ft_strlcat(char *dest, const char *src, size_t count);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t count);
+char	*ft_strnstr(const char *str, const char *find, size_t len);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
