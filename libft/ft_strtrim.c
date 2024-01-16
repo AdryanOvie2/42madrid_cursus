2eaa3b5f79c2cc7c4d540fcc64b4e2cd6e5fb1df
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-16 14:54:43 +0100

    function strtrim

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 14:01:27 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/01/16 14:01:28 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+	char	*mem;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (strchr(set, *s1) && *s1 != '\0')
+		s1++;
+	if (*s1 == '\0')
+		return (ft_strdup(""));
+	len = ft_strlen(s1);
+	while (ft_strchr(set, s1[len]))
+		len--;
+	mem = ft_substr(s1, 0, len + 1);
+	return (mem);
+}
