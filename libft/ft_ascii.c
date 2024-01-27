6abd67121dc571c264006af17ab94d639ff89af6
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 12:16:58 +0100

    Check COMPROBADO

--- libft/ft_ascii.c
+++ libft/ft_ascii.c
@@ -6,9 +6,11 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:15:03 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/10 12:15:06 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:16:42 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isascii(int c)
 {
@@ -16,3 +18,14 @@ int	ft_isascii(int c)
 		return (c);
 	return (0);
 }
+/*
+int	main(void)
+{
+	char	character = 128;
+	char	a = ft_isascii(character);
+	if (a)
+		printf("%c\n", a);
+	else
+		printf("Error, no es un caracter ascii\n");
+	return (0);
+}*/
