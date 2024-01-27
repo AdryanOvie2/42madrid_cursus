763ee6ca2106e04d982cac99a2c75b42edd9ba85
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 12:05:35 +0100

    Check COMPROBADA

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -6,13 +6,23 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:21:30 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/10 12:21:33 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:05:25 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
-		c += 22;
+		c += 32;
 	return (c);
 }
+/*
+int	main(void)
+{
+	char	letter = 'A';
+	char	to_lower = ft_tolower(letter);
+	printf("Letra convertida: %c\n", to_lower);
+	return (0);
+}*/
