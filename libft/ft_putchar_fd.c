0bd7f1cab2ebbe474427318c42bfafad0eb4bb30
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 14:16:53 +0100

    NI IDEA

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 22:14:22 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/16 22:14:39 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/27 14:16:41 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,13 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+/*
+int	main(void)
+{
+	const char	*name = "42Madrid";
+	while (*name != '\0')
+	{
+		
+	}
+	return (0);
+}*/
