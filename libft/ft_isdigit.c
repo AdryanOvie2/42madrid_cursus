eb388f3c4c92c948a19a6a92b7e2f46f409c1656
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 12:41:41 +0100

    Check COMPROBADA

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,9 +6,11 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:09:33 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/10 12:10:45 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:41:06 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isdigit(int c)
 {
@@ -16,3 +18,14 @@ int	ft_isdigit(int c)
 		return (c);
 	return (0);
 }
+/*
+int	main(void)
+{
+	char	ch = 50;
+	char whatis = ft_isdigit(ch);
+	if (whatis)
+		printf("El caracter digit es: %c\n", whatis);
+	else
+		printf("No es un caracter digit\n");
+	return (0);
+}*/
