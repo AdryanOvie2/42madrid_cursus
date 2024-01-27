76a6107821675d4e3ad5b05329ee5034f1c96fa6
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 12:39:19 +0100

    Check COMPROBADA

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,9 +6,11 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:17:02 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/10 12:17:04 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:38:58 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isprint(int c)
 {
@@ -16,3 +18,14 @@ int	ft_isprint(int c)
 		return (c);
 	return (0);
 }
+/*
+int	main(void)
+{
+	char	ch = 69;
+	char whatis = ft_isprint(ch);
+	if (whatis)
+		printf("El caracter print es: %c\n", whatis);
+	else
+		printf("No es un caracter print\n");
+	return (0);
+}*/
