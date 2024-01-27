3f30d09f283216ea3e5ca67642561e05e2dea925
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 21:55:11 +0100

    edit

--- libft/libft.h
+++ libft/libft.h
@@ -35,7 +35,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strhr(const char *s, int c);
 char	*ft_strdup(const char *str);
-char	*ft_strlcat(char *dest, const char *src, size_t count);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t count);
