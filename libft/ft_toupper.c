7cc3554ff3959b73d5f2b57105ea3e501a308ead
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-27 12:03:02 +0100

    Check COMPROBADA

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,13 +6,23 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:19:03 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/10 12:19:10 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:02:17 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_toupper(int c)
 {
 	if (c >= 97 && c <= 122)
-		c -= 22;
+		c -= 32;
 	return (c);
 }
+/*
+int	main(void)
+{
+	char	letter = 'a';
+	char	letter_convert = ft_toupper(letter);
+	printf("Letra convertida: %c\n", letter_convert);
+	return (0);
+}*/
