4ff8a206ca8932a8b5864d343b43d3e237c0b25e
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-31 14:23:09 +0100

    Check COMPROBADA

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:36:12 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/27 17:31:48 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/31 14:21:47 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(const char *s1, const char *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*dest;
 	size_t	len;
@@ -20,19 +20,18 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	len = (ft_strlen(s1) + ft_strlen(s2) + 1);
-	dest = (char *)malloc(sizeof(char) * len + 1);
+	dest = (char *)malloc(sizeof(char) * len);
 	if (!dest)
 		return (NULL);
 	ft_strlcpy(dest, s1, len);
-	ft_strlcat(dest, s1, len);
-	dest[len] = '\0';
+	ft_strlcat(dest, s2, len);
 	return (dest);
 }
 /*
 int	main(void)
 {
 	const char	*s1 = "UP ";
-	const char	*s2 = "To You";
+	const char	*s2 = "TO YOU";
 	char *to_join = ft_strjoin(s1, s2);
 	if (to_join != NULL)
 		printf("La cadena resultante es: '%s'\n", to_join);
