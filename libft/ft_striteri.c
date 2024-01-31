c35c1327e7eb58df5a40c819571cce6cf30ae2ed
AdryanOvie2 <adryanovie2@gmail.com>
2024-01-31 14:31:24 +0100

    Check COMPROBADA

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,12 +6,35 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:31:30 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/01/27 15:06:51 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/01/31 14:28:19 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_striteri(char *s, void (*f)(unsigned int, char*))
+static void	ft_putchar(unsigned int i, char *c)
 {
+	i += *c;
+	write(1, c, 1);
 }
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	i = 0;
+	if (s && f)
+	{
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
+}
+/*
+int main(void)
+{
+    char *str = "hola42Madrid";
+    ft_striteri(str, &ft_putchar);
+}*/
