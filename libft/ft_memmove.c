4822a04a746431b5fc0e07828cd7da5e17c5a7f9
AdryanOvie2 <adryanovie2@gmail.com>
2024-02-02 14:43:59 +0100

    Check PACO

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 20:49:45 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/02/02 14:39:58 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/02/02 14:43:30 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 			*dest_ptr++ = *src_ptr++;
 	return (dest);
 }
-/*
-int main(void)
-{
-	char *src = "123456789";
-	char *dest = "123456789";
-	printf("src: %s\n", src);
-	printf("dest: %s\n", dest);
-	ft_memmove(dest, src, 5);
-	printf("src: %s\n", src);
-	printf("dest: %s\n", dest);
-	return (0);
-}*/
