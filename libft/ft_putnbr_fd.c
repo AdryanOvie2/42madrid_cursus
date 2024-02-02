f1a4a81d929e08393994be106344ad5f34319995
AdryanOvie2 <adryanovie2@gmail.com>
2024-02-02 16:13:33 +0100

    Check PACO

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 22:21:06 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/02/01 23:55:46 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/02/02 16:11:22 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,30 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	r;
+	char	c;
 
-	r = n;
-	if (r < 0)
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
 	{
-		ft_putchar_fd('-', fd);
-		r = r * -1;
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n = -n;
+			ft_putnbr_fd(n, fd);
+		}
+		else if (n < 10)
+		{
+			c = n + '0';
+			write(fd, &c, 1);
+		}
+		else
+		{
+			ft_putnbr_fd(n / 10, fd);
+			c = (n % 10) + '0';
+			write(fd, &c, 1);
+		}
 	}
-	if (r >= 10)
-	{
-		ft_putnbr_fd(r / 10, fd);
-		ft_putnbr_fd(r % 10, fd);
-	}
-	ft_putchar_fd(r + '0', fd);
 }
 /*
 int	main(void)
