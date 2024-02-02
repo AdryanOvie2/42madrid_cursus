cef09fad2282ea4727781de424d5abceb85caac5
AdryanOvie2 <adryanovie2@gmail.com>
2024-02-02 16:07:00 +0100

    Check PACO

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,40 +6,46 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:35:20 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/02/01 23:40:18 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/02/02 16:05:12 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *find, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	size_t	c;
+	size_t	n_len;
+	char	*hay;
 
 	i = 0;
-	if (*find == '\0')
-		return ((char *)str);
-	while (*str != '\0' && len > 0)
+	hay = (char *)haystack;
+	n_len = ft_strlen(needle);
+	if (n_len == 0 || haystack == needle)
+		return (hay);
+	while (hay[i] != '\0' && i < len)
 	{
-		while (str[i] == find[i] && find[i] != '\0' && i < len)
-			i++;
-		if (find[i] == '\0')
-			return ((char *)str);
-		str++;
-		len--;
+		c = 0;
+		while (hay[i + c] != '\0' && needle[c] != '\0'
+			&& hay[i + c] == needle[c] && i + c < len)
+			c++;
+		if (c == n_len)
+			return (hay + i);
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
 /*
 int	main(void)
 {
 	const char *cadena = "Up To You!";
-    const char *subcadena = "To";
+    const char *subcadena = "Ya";
     size_t max_caracteres = 15;
     char *resultado = ft_strnstr(cadena, subcadena, max_caracteres);
     if (resultado != NULL)
         printf("La subcadena fue encontrada en: %s\n", resultado);
     else
-        printf("La subcadena no fue encontrada.\n");
+        printf("%s.\n", resultado);
     return (0);
 }*/
