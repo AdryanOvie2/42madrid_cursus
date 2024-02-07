ab9895829767cea76e7b68bbbe1b6ea27d9dcc4f
aoviedo- <adryanovie2@gmail.com>
2024-02-07 15:29:58 +0100

    Function Bonus Error

---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 15:25:39 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/02/07 15:26:10 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    del(lst->content);
+    free(lst);
+}
+/*
+int main(void)
+{
+    char *str = "Hello World!";
+    t_list *new = ft_lstnew(str);
+    printf("%s\n", (char *)new->content);
+    ft_lstdelone(new, free);
+    printf("%s\n", (char *)new->content);
+    return (0);
+}*/
