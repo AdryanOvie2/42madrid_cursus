dfb0faf981881c2b2986ec372f177a03bd877ecb
aoviedo- <adryanovie2@gmail.com>
2024-02-07 14:34:34 +0100

    Function Bonus Error

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoviedo- <aoviedo-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 13:02:07 by aoviedo-          #+#    #+#             */
+/*   Updated: 2024/02/07 13:02:17 by aoviedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list    *ft_lstnew(void *content)
+{
+    t_list	*nodo;
+
+    nodo = malloc(sizeof(t_list));
+    if (!nodo)
+        return (0);
+    nodo->content = content;
+    nodo->next = NULL;
+    return (nodo);
+}
+/*
+int main(void)
+{
+    char *str = "Hello World!";
+    t_list *new = ft_lstnew(str);
+    printf("%s\n", (char *)new->content);
+    return (0);
+}*/
