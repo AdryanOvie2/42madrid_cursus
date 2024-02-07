d9217dd9abc3b5c7b89d4a743646ee24606481c6
aoviedo- <adryanovie2@gmail.com>
2024-02-07 19:16:51 +0100

    paco bonus ✅

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aoviedo- <aoviedo-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 15:32:48 by aoviedo-          #+#    #+#             */
-/*   Updated: 2024/02/07 17:39:52 by aoviedo-         ###   ########.fr       */
+/*   Updated: 2024/02/07 19:15:54 by aoviedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new);
 }
-/*
-int main(void)
-{
-    char *str = "Hello World!";
-    t_list *new = ft_lstnew(str);
-    printf("%s\n", (char *)new->content);
-    t_list *new2 = ft_lstnew("Hello World2!");
-    ft_lstadd_back(&new, new2);
-    printf("%s\n", (char *)new->content);
-    printf("%s\n", (char *)new->next->content);
-    printf("%d\n", ft_lstsize(new));
-    printf("%s\n", (char *)ft_lstlast(new)->content);
-    t_list *new3 = ft_lstmap(new, f, free);
-    printf("%s\n", (char *)new3->content);
-    printf("%s\n", (char *)new3->next->content);
-    printf("%d\n", ft_lstsize(new3));
-    printf("%s\n", (char *)ft_lstlast(new3)->content);
-    return (0);
-}*/
